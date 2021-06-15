d2d320600697e9f30be55a0c3eb82a1e5b1f6d59
AlexP <alexander.pristavka@gmail.com>
2021-06-15 23:45:05 +0300

    Add basic fetch post request to create user;

--- js/script.js
+++ js/script.js
@@ -9,11 +9,29 @@ async function fetchGet( url ) {
   }
 }
 
+async function createUser( {name, username, email, website, ...rest} ) {
+  return await fetch( USERS_URL, {
+  method: 'POST',
+  body: JSON.stringify({
+    name,
+    username,
+    email,
+    website,
+    ...rest
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+}
+
 const columnHeaders = ['name','username','email','website'];
 
 
-( async(doc) => {
-  doc.body.innerHTML = `<table>
+( async(tableContainer) => {
+  tableContainer.innerHTML = `<table>
       <thead>
           <tr>
           ${columnHeaders.map( header=>`<th>${header}</th>`).join('')}
@@ -23,10 +41,51 @@ const columnHeaders = ['name','username','email','website'];
           ${( await fetchGet( USERS_URL ) )
             .map( user => `<tr>${columnHeaders
               .map( prop =>`<td>${user[prop]}</td>`)
-              .join('')}</tr>` )
+              .join('')} <td><button onclick="console.log('delete');">X</button></td></tr>` )
             .join('') }
-      </tbody>
+      </tbody>)
   </table>
   `
   return null;
-} )(document);
+} )(tableContainer);
+
+window.onload = function() {
+  btnCreateUser.addEventListener( 'click', ()=>{
+    createUser({
+      name: 'Jane Doe',
+      username: 'janedoe21',
+      email : 'janedoe@test.com',
+      website : 'http://janedoe.testdomain.com',
+    //   "address": {
+    //   "street": "Kulas Light",
+    //   "suite": "Apt. 556",
+    //   "city": "Gwenborough",
+    //   "zipcode": "92998-3874",
+    //   "geo": {
+    //     "lat": "-37.3159",
+    //     "lng": "81.1496"
+    //   }
+    // },
+    // "phone": "1-770-736-8031 x56442",
+    });
+
+    // createUser({
+    //   name: 'Jane Doe',
+    //   username: 'janedoe21',
+    //   email : 'janedoe@test.com',
+    //   website : 'http://janedoe.testdomain.com',
+    //   "address": {
+    //   "street": "Kulas Light",
+    //   "suite": "Apt. 556",
+    //   "city": "Gwenborough",
+    //   "zipcode": "92998-3874",
+    //   "geo": {
+    //     "lat": "-37.3159",
+    //     "lng": "81.1496"
+    //   }
+    // },
+    // "phone": "1-770-736-8031 x56442",
+    // });
+  } )
+}
+
